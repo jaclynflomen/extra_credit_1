0cb288338fd17401df3594e84527e29715c480cb
jaclynflomen <j_floman@fanshaweonline.ca>
2018-12-14 17:55:58 -0500

    updating images

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var headerIMG = document.getElementById('img');
+headerIMG.src = ("images/eEdu_profile.jpg");
+
+var topImg1 = document.getElementById('topImg');
+topImg1.src = ("images/eEdu_mainImg.jpg");
+
+var prof1 = document.getElementById('prof');
+prof1.src = ("images/eEdu_prof.jpg");
+
+var logo = document.getElementById('logo1');
+logo.src = ("images/eEdu_ANUC_logo.jpg");
+
+
+// })();
